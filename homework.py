a3e2faa44f00bc454d0aa055f991ce464c2f002c
Степан Калинин <stepan@onepk.ru>
2022-02-01 23:24:04 +0300

    Вторая попытка

--- homework.py
+++ homework.py
@@ -7,19 +7,19 @@ from dataclasses import dataclass, asdict
 class InfoMessage:
     """Информационное сообщение о тренировке."""
     training_type: str
-    duration_h: float
+    duration: float
     distance: float
     speed: float
     calories: float
     TEMPLATE = (
-        'Тип тренировки: {training_type}'
-        ' Длительность: {duration_h:.3f} ч.;'
+        'Тип тренировки: {training_type};'
+        ' Длительность: {duration:.3f} ч.;'
         ' Дистанция: {distance:.3f} км;'
         ' Ср. скорость: {speed:.3f} км/ч;'
         ' Потрачено ккал: {calories:.3f}.'
     )
 
-    def get_message(self) -> None:
+    def get_message(self) -> str:
         return self.TEMPLATE.format(**asdict(self))
 
 
